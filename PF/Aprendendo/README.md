36bb8b428ac8491abd91441dd4393006d41173b5
Kaio Souza <kaioaugusto02@gmail.com>
2021-03-05 11:05:00 -0300

    📝 Primeira atividade PF

---
+++ PF/Aprendendo/README.md
@@ -0,0 +1,1 @@
+Apenas alguns arquivos para aprendizado antes do início da matéria
---
+++ PF/Exercicios/Atividade 01/atividade01.hs
@@ -0,0 +1,12 @@
+-- Escreva uma função para calcular o dobro de um número.
+
+double n = n * 2
+
+-- Escreva uma função para quadruplicar um número usando a função dobro definida no item anterior.
+quadruple n = double n * 2
+
+-- Escreva uma função que, dadas as medidas dos catetos de um triângulo retângulo, retorne o valor de sua hipotenusa.
+hypotenuse a b = sqrt(a^2 + b^2)
+
+-- Escreva uma função para calcular a distância entre dois pontos A e B num plano cartesiano. 
+distance x1 y1 x2 y2 = sqrt((x2 - x1)^2 + (y2 - y1)^2)
